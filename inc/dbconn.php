d2e5cebc7ccc8e1558a49a157e53afae961a6d8b
melbwebdesign <leigh@melbournewebsitedesigns.net.au>
2015-10-10 22:44:11 +1100

    Update dbconn.php

--- inc/dbconn.php
+++ inc/dbconn.php
@@ -1,4 +1,16 @@
 <?php
+/**
+ * AccountRightV2 API class
+ *
+ * Development Center AccountRight Live API v2: http://developer.myob.com/api/accountright/v2/
+ * 
+ * THIS CLASS IS NOT IN ANY WAY AFFILIATED WITH MYOB OR ACCOUNTRIGHT
+ *
+ * @author Leigh Morrow <https://github.com/melbwebdesigns>
+ * @version 1.0
+ *
+ */
+
 require_once(__DIR__.'/includes.inc.php');
 
 try {
