060d88a40930a2d7bced3f2ebf26f4468e4bedda
melbwebdesign <leigh@melbournewebsitedesigns.net.au>
2015-10-10 22:33:37 +1100

    Create oauth.php

---
+++ oauth.php
@@ -0,0 +1,22 @@
+<?php
+require_once('config.php');
+
+require_once('src/AccountRightV2.php');
+
+use Myob\AccountRightV2\AccountRightV2;
+
+$accountRight = new AccountRightV2($myobConfig);
+
+if(isset($_GET['code'])) {
+	$json = $accountRight->getAccessToken($_GET['code']);
+
+	// Set Company File
+	$accountRight->getCompanyFile('COMPANY FILE NAME');
+
+	header('Location: index.php');
+	
+	exit;
+} else {
+	// TODO: Handle Exception
+	die('No code found');
+}
