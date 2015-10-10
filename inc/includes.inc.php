d01760cf557e8dadebc24171cd959d565afa1510
melbwebdesign <leigh@melbournewebsitedesigns.net.au>
2015-10-10 22:36:40 +1100

    Update includes.inc.php

--- inc/includes.inc.php
+++ inc/includes.inc.php
@@ -1,4 +1,20 @@
 <?php
+
+session_start();
+
 define('database','');
 define('username','');
 define('password','');
+
+// Set in your MYOB developer console
+$myobConfig['apiKey'] = '';
+$myobConfig['apiSecret'] = '';
+$myobConfig['apiCallback'] = 'http://yoursiteurl.com.au/oauth.php';
+// MYOB my.myob login
+$myobConfig['username'] = '';
+// MYOB my.myob password
+$myobConfig['password'] = '';
+// Specific company file username
+$myobConfig['cfusername'] = 'Administrator';
+// Specific company file password
+$myobConfig['cfpassword'] = '';
