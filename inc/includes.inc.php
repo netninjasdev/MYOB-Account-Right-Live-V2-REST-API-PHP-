34d2f43184fba50bc8228dd13374f60f487b2324
melbwebdesign <leigh@melbournewebsitedesigns.net.au>
2015-10-10 22:43:57 +1100

    Update includes.inc.php

--- inc/includes.inc.php
+++ inc/includes.inc.php
@@ -1,4 +1,15 @@
 <?php
+/**
+ * AccountRightV2 API class
+ *
+ * Development Center AccountRight Live API v2: http://developer.myob.com/api/accountright/v2/
+ * 
+ * THIS CLASS IS NOT IN ANY WAY AFFILIATED WITH MYOB OR ACCOUNTRIGHT
+ *
+ * @author Leigh Morrow <https://github.com/melbwebdesigns>
+ * @version 1.0
+ *
+ */
 
 session_start();
 
